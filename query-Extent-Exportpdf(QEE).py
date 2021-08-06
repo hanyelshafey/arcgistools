f4297515723ba75d8a475a69f0ce78b03191758e
hanyelshafey <hanyelshafey@outlook.com>
2021-08-06 22:49:39 +0200

    add input

--- query-Extent-Exportpdf(QEE).py
+++ query-Extent-Exportpdf(QEE).py
@@ -1,16 +1,19 @@
-﻿# coding: utf-8
+﻿# Enter your Queary
+lyrname = input ("Enter your id")
+row_id = input ("Enter your id")
+
+# Set arcgisproject , main Map
 aprx = arcpy.mp.ArcGISProject("CURRENT")
-mainMap = aprx.listMaps("Main Map")[0]
-mainMap.name
+mainMap = aprx.listMaps("Map")[0]
+
 # 'Main Map'
 lyt = aprx.listLayouts()[0]
 lyt.name
 # 'Layout1'
-countriesLayerMain = mainMap.listLayers("Countries")[0]
-countriesLayerMain.definitionQuery = "NAME = 'Egypt'"
+countriesLayerMain = mainMap.listLayers(f"{lyrname}")[0]
+countriesLayerMain.definitionQuery = f"id = '{row_id}'"
 mainMapFrame = lyt.listElements('MAPFRAME_ELEMENT',"Main Map Frame")[0]
 selCountryExtent = mainMapFrame.getLayerExtent(countriesLayerMain)
 mainMapFrame.camera.setExtent(selCountryExtent)
 mainMapFrame.camera.scale=mainMapFrame.camera.scale * 1.05
-lyt.exportToPDF(r"G:\GIS\arcpy\UDEMY ARCPY\test.pdf")
-# 'G:\\GIS\\arcpy\\UDEMY ARCPY\\test.pdf'
+

3b53edd0d653ba34839df203ffc5cbe5153fa2bc
hanyelshafey <hanyelshafey@outlook.com>
2021-08-07 14:14:09 +0200

    add id and parcel

--- query-Extent-Exportpdf(QEE).py
+++ query-Extent-Exportpdf(QEE).py
@@ -2,17 +2,18 @@
 row_id = input ("Enter your id")
 
 # Set arcgisproject , main Map
-aprx = arcpy.mp.ArcGISProject("CURRENT")
-mainMap = aprx.listMaps("Map")[0]
-
-# 'Main Map'
-lyt = aprx.listLayouts()[0]
-lyt.name
-# 'Layout1'
-parcelLayerMain = mainMap.listLayers("parcel")[0]
-parcelLayerMain.definitionQuery = f"parcel_id = '{row_id}'"
-mainMapFrame = lyt.listElements('MAPFRAME_ELEMENT',"Map Frame")[0]
-selParcelExtent = mainMapFrame.getLayerExtent(parcelLayerMain)
-mainMapFrame.camera.setExtent(selParcelExtent)
-mainMapFrame.camera.scale=mainMapFrame.camera.scale * 1.05
-lyt.exportToJPEG(f"G:\{row_id}.jpg")
+try:
+    aprx = arcpy.mp.ArcGISProject("CURRENT")
+    mainMap = aprx.listMaps("Map")[0]
+    # 'Main Map'
+    lyt = aprx.listLayouts()[0]
+    # 'Layout1'
+    parcelLayerMain = mainMap.listLayers("parcel")[0]
+    parcelLayerMain.definitionQuery = f"parcel_id = '{row_id}'"
+    mainMapFrame = lyt.listElements('MAPFRAME_ELEMENT',"Map Frame")[0]
+    selParcelExtent = mainMapFrame.getLayerExtent(parcelLayerMain)
+    mainMapFrame.camera.setExtent(selParcelExtent)
+    mainMapFrame.camera.scale=mainMapFrame.camera.scale * 1.05
+    lyt.exportToJPEG(f"G:\{row_id}.jpg")
+except:
+    print("Check your layout and layer name 'parcel' and field name 'parcel_id'")
